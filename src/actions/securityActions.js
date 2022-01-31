4ec705fba287fb8c1b75d76c112499b0abd95bac
congnguyenhcl <96488290+congnguyenhcl@users.noreply.github.com>
2022-01-30 21:01:10 -0500

    branch78 - created action for expired token to handle logout

--- src/actions/securityActions.js
+++ src/actions/securityActions.js
@@ -43,3 +43,11 @@ export const login = LoginRequest => async dispatch => {
     });
   }
 };
+export const logout = () => dispatch => {
+  localStorage.removeItem("jwtToken");
+  setJWTToken(false);
+  dispatch({
+    type: SET_CURRENT_USER,
+    payload: {}
+  });
+};
